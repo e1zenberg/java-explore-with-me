fa49989da373ba10ea509d8ae1b7727df2892154
e1zenberg <e1zenbergdev@gmail.com>
2025-10-12 15:07:58 +0300

    Сервис статистики v 1.1 правки чекстайла

--- stats/stats-server/src/main/java/ru/practicum/stats/server/DateFormats.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/DateFormats.java
@@ -3,7 +3,10 @@ package ru.practicum.stats.server;
 import java.time.format.DateTimeFormatter;
 
 public final class DateFormats {
-    private DateFormats() {}
+
     public static final DateTimeFormatter DATE_TIME =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private DateFormats() {
+    }
 }
--- stats/stats-server/src/main/java/ru/practicum/stats/server/model/Hit.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/model/Hit.java
@@ -1,20 +1,39 @@
 package ru.practicum.stats.server.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "hits", indexes = {
-        @Index(name = "idx_hits_uri", columnList = "uri"),
-        @Index(name = "idx_hits_timestamp", columnList = "timestamp"),
-        @Index(name = "idx_hits_ip", columnList = "ip")
-})
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor @Builder
+@Table(
+        name = "hits",
+        indexes = {
+                @Index(name = "idx_hits_uri", columnList = "uri"),
+                @Index(name = "idx_hits_timestamp", columnList = "timestamp"),
+                @Index(name = "idx_hits_ip", columnList = "ip")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Hit {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 100)
