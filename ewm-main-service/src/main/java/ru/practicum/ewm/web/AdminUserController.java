64ae60c89b53cdad7d69d998f10e95d7e513547f
e1zenberg <e1zenbergdev@gmail.com>
2025-10-17 16:52:36 +0300

    v2.2 добиваю волидацию

--- ewm-main-service/src/main/java/ru/practicum/ewm/web/AdminUserController.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/web/AdminUserController.java
@@ -1,17 +1,24 @@
 package ru.practicum.ewm.web;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.ewm.dto.NewUserRequest;
 import ru.practicum.ewm.dto.UserDto;
 import ru.practicum.ewm.service.UserService;
 import ru.practicum.ewm.util.PageUtils;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +30,7 @@ public class AdminUserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDto create(@RequestBody @Valid NewUserRequest dto) {
+    public UserDto create(@Valid @RequestBody NewUserRequest dto) {
         return userService.create(dto);
     }
 
